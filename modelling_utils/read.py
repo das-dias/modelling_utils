283c43a8220cdf9a4e961d99bf6b24e56d69c6bc
dasdias <das.dias6@gmail.com>
2022-05-06 18:04:49 +0100

    Write and reading info and data functionalities implemented.

--- modelling_utils/read.py
+++ modelling_utils/read.py
@@ -2,10 +2,12 @@ from loguru import logger
 import os
 import yaml
 import json
-
-def _read(path:str) -> dict:
+import csv
+import pandas as pd
+def read(path:str) -> dict:
     """_summary_
-    
+    Reads the contents of YAML and JSON files and returns a dictionary
+    containing the labelled information from within the files
     Args:
         path (str): path to read the file from
 
@@ -21,7 +23,7 @@ def _read(path:str) -> dict:
         raise FileNotFoundError(f"File {path} not found")
     head,tail = os.path.split(path)
     name, extension = os.path.splitext(tail)
-    if extension not in [ ".yaml", ".yml", ".json" ]:
+    if extension not in [ ".yaml", ".yml", ".json"]:
         raise ValueError(f"File {path} is not a valid specification file. Only .yaml, .yml, .json are accepted")
     struct = {}
     try:
@@ -29,29 +31,41 @@ def _read(path:str) -> dict:
             if extension in [".yaml", ".yml"]:
                 struct = yaml.safe_load(file)
             elif extension == ".json":
-                specs = json.load(file)
+                struct = json.load(file)
     except:
         raise IOError(f"File {path} could not be read")
     return struct
 
 def read_specs(path: str) -> dict:
-    try:
-        specs = _read(path)
-    except:
-        logger.error(f"Could not read specs from {path}")
-        return None
-    logger.info(f"Read specs from {path}")
-    # test if the specification fields are valid
-    #...
-    return specs # if all is well, return the specs
+    raise NotImplementedError("read_specs is not implemented")
 
 def read_model(path: str) -> dict:
+    raise NotImplementedError("read_model is not implemented")
+
+def read_data(path: str) -> pd.DataFrame:
+    """_summary_
+    Reads a CSV data file and returns a pandas dataframe
+    Args:
+        path (str): path to read the file from
+
+    Raises:
+        FileNotFoundError: _description_
+        ValueError: _description_
+        IOError: _description_
+
+    Returns:
+        pandas DataFrame: dataframe containing the extracted information from the CSV file
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File {path} not found")
+    head,tail = os.path.split(path)
+    name, extension = os.path.splitext(tail)
+    if extension !=  ".csv":
+        raise ValueError(f"File {path} is not a valid specification file. Only .csv files are accepted")
+    df = None
     try:
-        model = _read(path)
+        with open(path, 'r') as file:
+            df = pd.read_csv(file)
     except:
-        logger.error(f"Could not read model from {path}")
-        return None
-    logger.info(f"Read model from {path}")
-    # test if the specification fields are valid
-    #...
-    return model # if all is well, return the specs
+        raise IOError(f"File {path} could not be read")
+    return df
